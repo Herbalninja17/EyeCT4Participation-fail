c859dcd7d63f3ed750650dcd14f407912b6a05d9
Olaf van Dijk <336692@student.fontys.nl>
2016-04-13 09:45:38 +0200

    Goede versie

--- EyeCT4Participation/EyeCT4Participation/Business/Request.cs
+++ EyeCT4Participation/EyeCT4Participation/Business/Request.cs
@@ -8,5 +8,24 @@ namespace EyeCT4Participation.Business
 {
     class Request
     {
+        public int requestID { get; set; }
+
+        public int needyID { get; set; }
+
+        public string Description { get; set; }
+
+        public bool urgency { get; set; }
+
+        public string location { get; set; }
+
+        public int travelTime { get; set; }
+
+        public DateTime startDate { get; set; }
+
+        public DateTime endDate { get; set; }
+
+        public int totalVolunteer { get; set; }
+
+        public List<string> reactionlist { get; set; }
     }
 }
--- EyeCT4Participation/EyeCT4Participation/Business/Review.cs
+++ EyeCT4Participation/EyeCT4Participation/Business/Review.cs
@@ -8,5 +8,20 @@ namespace EyeCT4Participation.Business
 {
     class Review
     {
+        public int score { get; set; }
+
+        public string review { get; set; }
+
+        public int volunteerID { get; set; }
+
+        public bool reported { get; set; }
+
+        public Review(int score, string review, int volunteerID, bool reported)
+        {
+            this.score = score;
+            this.review = review;
+            this.volunteerID = volunteerID;
+            this.reported = reported;
+        }
     }
 }
